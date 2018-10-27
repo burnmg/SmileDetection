0679665c442c68196b447ccd62431d6af92aca75
burnmg <rlin1992@hotmail.com>
2018-10-27 19:02:24 -0400

    casda

--- test_form.py
+++ test_form.py
@@ -1,9 +1,16 @@
-import pytest
+import unittest
+
+class TestForm(unittest.TestCase):
 
 
-def test_capital_case():
-    assert 1 == 1
+    def test_capital_case1(self):
+        self.assertTrue(True)
 
+    def test_capital_case2(self):
+        self.assertTrue(True)
 
-def test_2():
-    assert 1 == 1
+    def test_capital_case3(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
